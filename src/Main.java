67737a6fc59a52e63deaa5946d10ae1170d1ff90
Clock <mdsajjadhossain614@gmail.com>
2023-09-07 13:33:15 +0600

    new commit.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //loginPage loginPage = new loginPage();
+
         SignupPage signupPage = new SignupPage();
+        //loginPage loginPage = new loginPage();
         //homePage homePage = new homePage();
     }
 }
